bc4cda4134bb207f40603d0a7ecb5b3b50d2b552
Sam Morrow <sam@clubhausagency.com>
2025-10-04 15:40:00 -0500

    Add Scandia Stencil font and apply to all titles
    - Add new Adobe Fonts kit (fju6rnv) for Scandia Stencil font
    - Update AdobeFonts component to load both font kits
    - Replace Proxima Nova with Scandia Stencil for all headings (h1-h6)
    - Apply Scandia Stencil to SplitText components
    - Font weight: 700, style: normal
    - Body text remains Halcom

--- src/components/AdobeFonts.tsx
+++ src/components/AdobeFonts.tsx
@@ -4,15 +4,21 @@ import { useEffect } from 'react';
 
 export default function AdobeFonts() {
   useEffect(() => {
-    // Create and inject the Adobe Fonts CSS link
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://use.typekit.net/qhq4tmi.css';
-    document.head.appendChild(link);
+    // Create and inject the first Adobe Fonts CSS link (Halcom, Proxima Nova)
+    const link1 = document.createElement('link');
+    link1.rel = 'stylesheet';
+    link1.href = 'https://use.typekit.net/qhq4tmi.css';
+    document.head.appendChild(link1);
 
-    // Adobe Fonts loading script
-    const script = document.createElement('script');
-    script.innerHTML = `
+    // Create and inject the second Adobe Fonts CSS link (Scandia Stencil)
+    const link2 = document.createElement('link');
+    link2.rel = 'stylesheet';
+    link2.href = 'https://use.typekit.net/fju6rnv.css';
+    document.head.appendChild(link2);
+
+    // Adobe Fonts loading script for first kit
+    const script1 = document.createElement('script');
+    script1.innerHTML = `
       (function(d) {
         var config = {
           kitId: 'qhq4tmi',
@@ -22,16 +28,28 @@ export default function AdobeFonts() {
         h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
       })(document);
     `;
-    document.head.appendChild(script);
+    document.head.appendChild(script1);
+
+    // Adobe Fonts loading script for second kit
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      (function(d) {
+        var config = {
+          kitId: 'fju6rnv',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    `;
+    document.head.appendChild(script2);
 
     return () => {
       // Cleanup
-      if (link.parentNode) {
-        link.parentNode.removeChild(link);
-      }
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
+      if (link1.parentNode) link1.parentNode.removeChild(link1);
+      if (link2.parentNode) link2.parentNode.removeChild(link2);
+      if (script1.parentNode) script1.parentNode.removeChild(script1);
+      if (script2.parentNode) script2.parentNode.removeChild(script2);
     };
   }, []);
 
